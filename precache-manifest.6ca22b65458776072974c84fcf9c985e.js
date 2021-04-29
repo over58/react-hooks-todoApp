16243dee2f5360076569cad67c9ac423f048d959
over58 <26474133+over58@users.noreply.github.com>
2021-04-29 11:41:48 +0000

    Deploying to gh-pages from  @ 91dbb6557034a73f71feb999b82a4c58d90ef2d8 🚀

---
+++ precache-manifest.6ca22b65458776072974c84fcf9c985e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0803a274b16ae2b7f7e50be59e9064c2",
+    "url": "/react-hooks-todolist/index.html"
+  },
+  {
+    "revision": "b57b6a36e0c60e150777",
+    "url": "/react-hooks-todolist/static/css/main.a0b496bd.chunk.css"
+  },
+  {
+    "revision": "8162bc3e461a910146d4",
+    "url": "/react-hooks-todolist/static/js/2.ebc50fc1.chunk.js"
+  },
+  {
+    "revision": "b57b6a36e0c60e150777",
+    "url": "/react-hooks-todolist/static/js/main.c5bfaf7e.chunk.js"
+  },
+  {
+    "revision": "1ebd81261b86a19dc9be",
+    "url": "/react-hooks-todolist/static/js/runtime-main.dfe36d5f.js"
+  }
+]);
