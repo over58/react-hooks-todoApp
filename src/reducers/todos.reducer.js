b8732199690fe212b8136dfcb0d6e0fc710c77dd
xuyongchao6585 <xuyongchao6585@ipalfish.com>
2021-04-29 19:37:24 +0800

    feat: add uuid

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -1,28 +1,28 @@
-import uuid from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid'
 import {
   ADD_TODO,
   REMOVE_TODO,
   TOGGLE_TODO,
-  EDIT_TODO
-} from '../constants/actions';
+  EDIT_TODO,
+} from '../constants/actions'
 
 const reducer = (state, action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { id: uuid(), task: action.task, completed: false }];
+      return [...state, { id: uuidv4(), task: action.task, completed: false }]
     case REMOVE_TODO:
-      return state.filter(todo => todo.id !== action.id);
+      return state.filter((todo) => todo.id !== action.id)
     case TOGGLE_TODO:
-      return state.map(todo =>
+      return state.map((todo) =>
         todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
-      );
+      )
     case EDIT_TODO:
-      return state.map(todo =>
+      return state.map((todo) =>
         todo.id === action.id ? { ...todo, task: action.task } : todo
-      );
+      )
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default reducer;
+export default reducer
